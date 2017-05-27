255f8254afe4dd8a3b705a51fd8cd45e99e2de1f
Damian Galindo <damian.galindo@koombea.com>
2017-05-27 09:38:16 -0500

    course 2

---
+++ book_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript', pages: 100 },
+    { title: 'Harry Potter', pages: 102 },
+    { title: 'The dark tower', pages: 180 },
+    { title: 'Eloquent ruby', pages: 1 }
+  ]
+}
